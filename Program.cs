69ba83e87bf47e19be4c1bea7e646646e375f2c1
Tipimenta <tiagopimenta.ata@gmail.com>
2024-04-27 09:35:42 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int pessoas = 5;
+string[] nome = new string[pessoas];
+int[] idades = new int[pessoas];
+int maiores = 0;
+
+for (int i = 0; i < pessoas; i++)
+{
+    Console.WriteLine("Digite seu nome:");
+    nome[i] = Console.ReadLine();
+    Console.Clear();
+
+    Console.WriteLine($"{nome[i]} digite sua idade:");
+    idades[i] = int.Parse(Console.ReadLine());
+    Console.Clear();
+
+    if (idades[i] >= 18)
+        maiores++;
+}
+
+Console.WriteLine($"número de pessoas maiores de idade = {maiores} ");
